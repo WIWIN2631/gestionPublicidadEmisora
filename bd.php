a3007c9f34432d4bf3c8a30b12423c1f9454b236
Brayan Cardona <cardonacardonabrayan935@gmail.com>
2026-03-28 16:02:35 -0500

    conexion bd

---
+++ bd.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$bdname = 'gestionpublicidademisora';
+$user = 'root';
+$pass = '';
+
+try { 
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+    $conexionBd = mysqli_connect($host, $user, $pass, $bdname);
+    mysqli_set_charset($conexionBd, "utf8mb4");
+
+} catch (mysqli_sql_exception $error) {
+    header("Content-type: application/json");
+    echo json_encode(["ok" => false, "mensaje"=> "Error de conexión: ".$error->getMessage()]);
+    exit;
+}
